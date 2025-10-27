9b1cb00a66864ad731c8e66bc34c7479909bd472
chonees <chonees@example.com>
2025-10-27 04:07:11 -0300

    front end stuff

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,8 @@ app = FastAPI(title="AI Customer Support Agent")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174"
 ]
 app.add_middleware(
     CORSMiddleware,
